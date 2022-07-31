45e4c74e9f41e15d146ec9c13492b348d914d7b2
Natalia Miccini <natimiccini@hotmail.it>
2022-07-31 16:18:46 +0200

    Create connection to db with Singleton pattern

---
+++ src/connection/Singleton.ts
@@ -0,0 +1,30 @@
+require('dotenv').config();
+import { Sequelize } from 'sequelize';
+
+/**
+ * Classe 'SequelizeSingleton'
+ * 
+ * Classe che si occupa di assicurare la presenza di una singola istanza di un oggetto durante il 
+ * ciclo di vita del servizio. L'oggetto è utilizzato per costruire la connessione al database
+ * attraverso la libreria {@link Sequelize}.
+ */
+export class Singleton {
+	
+    private static instance: Singleton;
+	private connection: Sequelize;
+
+    private constructor() {
+		this.connection = new Sequelize("graph", "user", "root", {
+			host: "dbmysql",
+			port: 3306 ,
+			dialect: 'mysql'
+		});
+	}
+
+	public static getConnection(): Sequelize {
+        if (!Singleton.instance) {
+            this.instance = new Singleton();
+        }
+        return Singleton.instance.connection;
+    }
+}
