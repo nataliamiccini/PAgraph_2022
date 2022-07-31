cfa4fde18e02a3e00dfcfe68df66afac3a3171a9
Natalia Miccini <natimiccini@hotmail.it>
2022-07-31 15:55:54 +0200

    Create graph.sql

---
+++ db/graph.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Graph (
+    id_graph int(11) NOT NULL, 
+    node_a varchar(100) NOT NULL,
+    node_b varchar(100) NOT NULL,
+    weight_edge int(11) NOT NULL,
+    modify_date varchar(100) NOT NULL
+) 
+
+ALTER TABLE user
+  ADD PRIMARY KEY (id_graph);
+
+INSERT INTO Graph (id_graph, node_a, node_b, weight_edge, modify_date) VALUES
+  (1,'A', 'B', 3, '2022-07-31T15:40:00+01:00'),
+  (1,'B', 'C', 10, '2022-07-31T15:40:00+01:00'),
+  (1,'C', 'D', 5, '2022-07-31T15:40:00+01:00'),
+  (1,'A', 'E', 2, '2022-07-31T15:40:00+01:00'),
+  (1,'D', 'E', 1, '2022-07-31T15:40:00+01:00'),
