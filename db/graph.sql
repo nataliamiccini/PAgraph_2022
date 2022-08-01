c71e8d926a912b34e882f2e3725dcd0a81567015
AsyaPes <pesaresiasya@gmail.com>
2022-08-01 10:29:48 +0200

    update database

--- db/graph.sql
+++ db/graph.sql
@@ -1,4 +1,5 @@
 CREATE TABLE graph (
+    id_edge varchar(100) NOT NULL, 
     id_graph int(11) NOT NULL, 
     node_a varchar(100) NOT NULL,
     node_b varchar(100) NOT NULL,
@@ -7,11 +8,11 @@ CREATE TABLE graph (
 ); 
 
 ALTER TABLE graph
-  ADD PRIMARY KEY (id_graph);
+  ADD PRIMARY KEY (id_edge);
 
-INSERT INTO graph (id_graph, node_a, node_b, weight_edge, modify_date) VALUES
-  (1,'A', 'B', 3, '2022-07-31T15:40:00+01:00'),
-  (1,'B', 'C', 10, '2022-07-31T15:40:00+01:00'),
-  (1,'C', 'D', 5, '2022-07-31T15:40:00+01:00'),
-  (1,'A', 'E', 2, '2022-07-31T15:40:00+01:00'),
-  (1,'D', 'E', 1, '2022-07-31T15:40:00+01:00');
+INSERT INTO graph (id_edge, id_graph, node_a, node_b, weight_edge, modify_date) VALUES
+  ('a1',1,'A', 'B', 3, '2022-07-31T15:40:00+01:00'),
+  ('b1',1,'B', 'C', 10, '2022-07-31T15:40:00+01:00'),
+  ('c1',1,'C', 'D', 5, '2022-07-31T15:40:00+01:00'),
+  ('d1',1,'A', 'E', 2, '2022-07-31T15:40:00+01:00'),
+  ('e1',1,'D', 'E', 1, '2022-07-31T15:40:00+01:00');
