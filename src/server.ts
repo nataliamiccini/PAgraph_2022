619973eb6ca445ba451d3f66d456da7449108484
AsyaPes <pesaresiasya@gmail.com>
2022-08-02 17:58:09 +0200

    Add function to create graph with root

--- src/server.ts
+++ src/server.ts
@@ -8,14 +8,20 @@ app.use(express.json());
 app.get('/all', function(req: any, res: any) {    
     Service.showALL( req, res);
 });
+app.get('/test', function(req: any, res: any) {    
+    Service.test(req.body.id_graph, res);
+});
 
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
+app.post('/createGraph',function(req: any, res: any) { 
+    const route = new Graph(req.body) 
+    console.log(route)
+    Service.createGraph(req.body,res);
+    res.json("Grafo creato")
 
-const route = new Graph({
-    'A': { 'B': 1 },
-    'B': { 'A': 1, 'C': 2, 'D': 4 }
-  })
-  console.log(route)
+});
 
 const a = new Map()
 a.set( 'A', { 'B': 1 })
--- src/services/service.ts
+++ src/services/service.ts
@@ -11,13 +11,67 @@ export function showALL(req: any, res: any) {
     });
 };
 
+let r: any;
 
-export function test(req:any,res:any){
-    let ar=[]
-    let r: any;
-     r =  sequelize.query(
-         "SELECT node_a,node_b FROM graph WHERE id_graph=1",
-       )
-    
-
+export function test(id_graph:number,res:any){
+    let elem=[];
+    Graph.findAll({attributes: ['node_a'],where:{id_graph: id_graph}}).then(arr=>{
+        res.json(arr);
+    });
 }
+
+
+export function createGraph(req:any,res:any){
+let costo=[];
+let nodi_esterni=[];
+let nodi_interni=[];
+let keys;
+let val;
+
+
+   keys = Object.keys(req);
+   val = Object.values(req);
+
+   Object.getOwnPropertyNames(req).forEach(
+    function (val1) {
+    nodi_esterni.push(val1);
+  });
+
+
+   for(var i in nodi_esterni){
+    nodi_interni=[];
+    costo=[];
+    createGraph(req[keys[i]],res)
+    
+    if((Object.getOwnPropertyNames(req[keys[i]])).length !==0){
+      Object.getOwnPropertyNames(req[keys[i]]).forEach(
+        function (x) {
+          nodi_interni.push(Object.values(x));
+        });
+
+        Object.values(req[keys[i]]).forEach(
+          function (y) {
+            costo.push(y)
+          });
+
+          
+    }     
+      if(nodi_interni.length!==0) 
+       { console.log(i + " ESTERNO "+ nodi_esterni[i])
+
+      for(let z in nodi_interni){
+        console.log(i +" INTERNO "+ nodi_interni[z]);
+        console.log(i+ " costo "+ costo[z]);
+        var ID = Math.random().toString(36);
+        
+           Graph.create({id_edge:ID,id_graph:10,node_a:nodi_esterni[i],node_b:String(nodi_interni[z]),weight_edge:costo[z],modify_date:"2022-07-31T15:40:00+01:00"}).then((arr)=>{
+          
+      console.log("fatto");
+    })
+        
+        }
+        }
+      
+      }
+   
+  } 
