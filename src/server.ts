6b9bc6d14aa62dd0e7cac20e1433c4b2790e1d3d
Natalia Miccini <natimiccini@hotmail.it>
2022-08-03 17:11:48 +0200

    Insert new function to find path

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,9 @@ import * as Service from './services/service'
 const app = express();
 
 app.use(express.json());
+
 app.get('/all', function(req: any, res: any) {    
-    Service.showALL( req, res);
+    Service.showAllGraph( req, res);
 });
 app.get('/test', function(req: any, res: any) {    
     Service.test(req.body.id_graph, res);
@@ -24,6 +25,10 @@ app.post('/createGraph',function(req: any, res: any) {
 
 });
 
-
+app.get('/path', async function(req: any, res: any) {    
+  await Service.path(req.body.id_graph, req.body.node_a, req.body.node_b, res).then( result => {
+    res.json(result)
+  })
+});
 
 app.listen(8080 ,()=> console.log("Listening on port 8080"))
--- src/services/service.ts
+++ src/services/service.ts
@@ -1,36 +1,24 @@
 import { User } from '../models/user-model';
 import { Graph } from '../models/graph-model';
-import { Model, Sequelize, where,QueryTypes } from 'sequelize';
+import { Model, Sequelize, where, QueryTypes } from 'sequelize';
 import { Singleton } from '../connection/Singleton';
 
+const Graph1 = require('node-dijkstra')
 const sequelize: Sequelize = Singleton.getConnection();
 
-export function showALL(req: any, res: any) {
-    Graph.findAll({}).then(arr=>{
-        console.log(arr);
-        res.json(arr);
-    });
+export function showAllGraph(req: any, res: any) {
+  Graph.findAll({}).then(arr=>{
+      console.log(arr);
+      res.json(arr);
+  });
 };
 
-
-export function test(id_graph:number,res:any){
-    let elem=[];
-    Graph.findAll({attributes: ['node_a'],where:{id_graph: id_graph}}).then(arr=>{
-        res.json(arr);
-    });
+export function test(id_graph:number, res:any){
+  let elem=[];
+  Graph.findAll({attributes: ['node_a'], where:{id_graph: id_graph}}).then( arr => {
+      res.json(arr);
+  });
 }
-
-/*export function findMAX(){
-  sequelize.query(
-    "SELECT MAX(id_graph) as max FROM graph",
-    {raw:true,
-      type:QueryTypes.SELECT}
-  ).then(arr=>{
-    console.log(arr.map(item=>(item as any).max)[0])
-    arr.map(item=>(item as any).max)[0];
-  })
-}*/
-
 
 let max=0;
 export function createGraph(req:any,res:any){
@@ -55,92 +43,85 @@ export function createGraph(req:any,res:any){
     
   });
 
-
   for(let i in nodi_esterni){
     nodi_interni=[];
     costo=[];
-    
-    
-
     createGraph(req[keys[i]],res)
-    
     if((Object.getOwnPropertyNames(req[keys[i]])).length !==0){
       Object.getOwnPropertyNames(req[keys[i]]).forEach(
         function (x) {
           nodi_interni.push(Object.values(x));
           //console.log(x)
           
-        });
+        }
+      );
 
       Object.values(req[keys[i]]).forEach(
         function (y) {
           costo.push(y)
-        });
-          //tot_i.push(Object.getOwnPropertyNames(req[keys[i]]))
+        }
+      );
     }     
-      if(nodi_interni.length!==0) {
-        //console.log(i + " ESTERNO "+ nodi_esterni[i])
-
-
-        //console.log("totale "+ tot)
-        for(let z in nodi_interni){
-            //console.log(i +" INTERNO "+ nodi_interni[z]);
-            //console.log(i+ " costo "+ costo[z]);
-            
-
-            let ID = Math.random().toString(36);
-
-            sequelize.query(
-              "SELECT MAX(id_graph) as max FROM graph",
-              {raw:true,
-                type:QueryTypes.SELECT}
-            ).then(arr=>{
-              //console.log(arr.map(item=>(item as any).max)[0])
-              max= arr.map(item=>(item as any).max)[0];
-             });
-             
-             tot_c.push(costo[z]);
-             n_archi=tot_c.length;
-            
-             n_interni=tot_i.length;
-             //if((tot_i.indexOf(nodi_interni[z]))== -1) tot_i.push(nodi_interni[z]);
-             tot_e.add(nodi_interni[z])
-             
-
-            Graph.create({id_edge:ID,id_graph:max+2,node_a:nodi_esterni[i],node_b:String(nodi_interni[z]),weight_edge:costo[z],modify_date:"2022-07-31T15:40:00+01:00",FKuser_id:"Wos78BnB09"}).then((arr)=>{
-                //console.log("fatto");
-                
-            });
-        //if((tot_e.indexOf()&&tot_e.indexOf(nodi_esterni[i]))== -1) tot_e.push(nodi_esterni[i]);
-          } 
+    if(nodi_interni.length!==0) {
+      for(let z in nodi_interni){
+        let ID = Math.random().toString(36);
+        sequelize.query(
+          "SELECT MAX(id_graph) as max FROM graph",
+          {raw:true,
+          type:QueryTypes.SELECT}
+        ).then(arr=>{
+        max= arr.map(item=>(item as any).max)[0];
+        });
         
-        }        
-
-      }
-
-      tot_e.add(nodi_esterni)
-      n_esterni=tot_e.size;
-      //n_interni=tot_i.length;
-      //console.log("tot_i "+ tot_i)
-      //console.log("esterni  "+ tot_e)
-      //console.log("esterni "+n_esterni+"interni "+n_interni+"archi "+n_archi)
-return tot=0.25*n_esterni+ 0.01*n_archi;
-
-     /*User.decrement({token:tot},{where:{id_user:"Wos78BnB09"}}).then(arr=>{
-        //console.log("Hai pagato un totale di  "+ 0.25*(n_esterni+n_interni) + "token");
-        res.json("Hai pagato un totale di  "+ tot + "token")
-         });*/
-
-   
-  } 
-
-  export function endGraph(req:any,res:any){
-      let x= createGraph(req,res)
-      console.log("totale costo "+x)
-      User.decrement({token:x},{where:{id_user:"Wos78BnB09"}}).then(arr=>{
-        //console.log("Hai pagato un totale di  "+ 0.25*(n_esterni+n_interni) + "token");
-        res.json("Hai pagato un totale di  "+ x + "token")
-      //.then((arr)=>{
-      });
-      //})
+        tot_c.push(costo[z]);
+        n_archi=tot_c.length;
+      
+        n_interni=tot_i.length;
+        tot_e.add(nodi_interni[z])
+             
+        Graph.create({id_edge:ID,id_graph:max+2,node_a:nodi_esterni[i],node_b:String(nodi_interni[z]),weight_edge:costo[z],modify_date:"2022-07-31T15:40:00+01:00",FKuser_id:"Wos78BnB09"}).then((arr)=>{
+        });
+      } 
+    }        
   }
+  tot_e.add(nodi_esterni)
+  n_esterni=tot_e.size;
+  return tot=0.25*n_esterni+ 0.01*n_archi;
+}; 
+
+export function endGraph(req:any,res:any){
+  let x= createGraph(req,res)
+  console.log("totale costo "+x)
+  User.decrement({token:x},{where:{id_user:"Wos78BnB09"}}).then(arr=>{
+    res.json("Hai pagato un totale di  "+ x + "token")
+  });
+}
+
+export async function findGraph(id_graph: number, res: any): Promise< Map<string, Map<string, number>>>{
+  let map2: Map<string, Map<string, number>> = new Map();
+  let nodi_testa= new Set;
+  await Graph.findAll({attributes: ['node_a', 'node_b', 'weight_edge'], where: {id_graph: id_graph}}).then(arr => {
+    for ( let i = 0; i < arr.length; i++){
+      nodi_testa.add(arr[i].getDataValue("node_a"));
+    }
+    nodi_testa.forEach( (x) => {
+      let map3: Map<string, number> = new Map();
+      for (let i = 0; i < arr.length; i++){
+        if (arr[i].getDataValue("node_a") === x){
+          map3.set(arr[i].getDataValue("node_b"), arr[i].getDataValue("weight_edge"))
+          map2.set(arr[i].getDataValue("node_a"), map3)
+        }
+      }
+    })
+  });
+  return map2
+};
+
+export async function path(req: number, node_a: string, node_b: string, res: any): Promise<Array<any>>{
+  let ar = []
+  await findGraph(req, res).then( arr => {
+    const route = new Graph1(arr)
+    ar = route.path(node_a, node_b, {cost: true})
+  })
+ return ar
+};
